5f73f8531859ece404496a64301fe3011fa8fd58
Lucas Wesley Fragoso <lucaswmmf@gmail.com>
2023-07-18 18:50:28 -0300

    feat: add welcome page

--- Tudu/src/app/app.module.ts
+++ Tudu/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { WelcomePageComponent } from './welcome-page/welcome-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Tudu/src/app/welcome-page/welcome-page.component.ts
+++ Tudu/src/app/welcome-page/welcome-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {faArrowUp} from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-welcome-page',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome-page.component.css']
 })
 export class WelcomePageComponent {
-
+ faArrowUp = faArrowUp;
 }
