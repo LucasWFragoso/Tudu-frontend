63259f9bfabe09752b0ea9447ef451212aa46ab6
Lucas Wesley Fragoso <lucaswmmf@gmail.com>
2023-07-18 13:40:16 -0300

    feat: add welcomePage component

--- Tudu/src/app/app.module.ts
+++ Tudu/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
